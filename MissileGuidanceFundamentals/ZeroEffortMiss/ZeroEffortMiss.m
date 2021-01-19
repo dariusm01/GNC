d9c12f17796d297207317e280600aea086e12a4f
Darius Mensah <dariusmensah@Dariuss-MacBook-Pro.local>
2021-01-19 15:17:40 -0500

    Added number of outputs

--- MissileGuidanceFundamentals/ZeroEffortMiss/ZeroEffortMiss.m
+++ MissileGuidanceFundamentals/ZeroEffortMiss/ZeroEffortMiss.m
@@ -45,24 +45,44 @@ LambdaDot = ZEM_sim.LOSRate.signals.values;
 accelCmmd = ZEM_sim.AccelG.signals.values;
 ZEM_PLOS = ZEM_sim.ZEM_PLOS.signals.values;
 
+MissileDistance = ZEM_sim.M1_final.signals.values/1000;
+MissileAltitude = ZEM_sim.M2_final.signals.values/1000;
+
+TargetDistance = ZEM_sim.T1_final.signals.values/1000;
+TargetAltitude = ZEM_sim.T2_final.signals.values/1000;
+
 % Plotting
 figure(1)
 plot(time,LambdaDot)
 grid on
 xlabel("Time (s)")
 ylabel("LOS Rate (rad/s)")
+title("Line of Sight Rate $\dot{\lambda}$",'interpreter','latex')
 
 figure(2)
 plot(time,ZEM_PLOS)
 grid on
 xlabel("Time (s)")
 ylabel("ZEM Perp to LOS (ft)")
+title("Zero Effort Miss Perpendicular to the Line of Sight")
 
 figure(3)
 plot(time(1:end-1)/Tf,accelCmmd(1:end-1))
 grid on
 xlabel("t/tf")
 ylabel("Missile Acceleration (G)")
+title("Acceleration of the Missle (G)")
+
+figure(4)
+plot(TargetDistance,TargetAltitude)
+grid on
+hold on
+plot(MissileDistance,MissileAltitude)
+title('Two-dimensional tactical missile-target engagement simulation')
+xlabel('Downrange (Kft)')
+ylabel('Altitude (KFt)')
+lgd = legend("Target","Interceptor",'Location','southeast');
+hold off
 
 function L = LeadAngle(Vt,Vm,Beta,Lambda)
 a = Vt*sin(Beta+Lambda);
